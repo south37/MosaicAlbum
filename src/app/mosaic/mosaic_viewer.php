918066920dd738f51a4e0aebca83b5dfba3e2016
1000_ch <masa91.rec@gmail.com>
2013-10-07 02:02:35 +0900

    minimum routing /mosaic/(album,regist,viewer)

--- src/app/mosaic/mosaic_viewer.php
+++ src/app/mosaic/mosaic_viewer.php
@@ -1,22 +1,8 @@
 <?php
-/**
- * サンプル welcomeコントローラー
- */
-$app->get('/', function() use ($app) {
-        $app->render('top/index.html.twig');
+//parameter無しのget
+$app->get('/mosaic/viewer', function() use ($app) {
+        $app->render('mosaic/mosaic_viewer.html.twig');
     })
-    ->name('welcome')
+    ->name('render_viewer')
 ;
 
-/**
- * Demo 用
- */
-$app->get('/hello/:name', function ($name) use ($app, $container) {
-        // テンプレートでコードを表示するために
-        $phpcode = $contents = highlight_file(__FILE__, TRUE);
-        $twigcode = file_get_contents($container['twig.templateDir'] . '/top/hello.html.twig');
-        $app->render('top/hello.html.twig', ['name' => $name, 'phpcode' => $phpcode, 'twigcode' => $twigcode]);
-    })
-    ->name('hello')
-    ->conditions(array('name' => '\w+'))
-;
