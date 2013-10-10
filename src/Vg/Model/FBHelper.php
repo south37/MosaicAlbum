6ffbfb1fd5c166b1824b156e24b0b18fd826d2e9
nantyoku <syukatsusouth@gmail.com>
2013-10-11 00:07:44 +0900

    closed #3 closed #4 closed $5 closed #6

--- src/Vg/Model/FBHelper.php
+++ src/Vg/Model/FBHelper.php
@@ -22,12 +22,85 @@ class FBHelper
 
     public function getUserId()
     {
-        return $this->facebook->getUser();
+        $userId = $this->facebook->getUser();
+        if ($userId) {
+            try {
+                // Proceed knowing you have a logged in user who's authenticated.
+                $this->$facebook->api('/me');
+            } catch (FacebookApiException $e) {
+                error_log($e);
+                $userId = 0;
+            }
+        }
+        return $userId;
     }
 
     public function getLoginUrl()
     {
-        $scope = 'user_photos, frinends_photos';
-        return $this->facebook->getLoginUrl($scope);
+        $scope = 'user_photos,frinends_photos';
+        $loginUrl = $this->facebook->getLoginUrl(['scope' => $scope]);
+        return $loginUrl;
+    }
+
+    public function getUserProfile()
+    {
+        $me = $this->facebook->api('/me?locale=ja_JP');
+        $this->facebook->setExtendedAccessToken();
+        
+        $userProfile = [
+            'fb_user_id'  => $me['id'],
+            'token'       => $this->facebook->getAccessToken(),
+            'name'        => $me['name'],
+            'fb_icon_url' => 'https://graph.facebook.com/'.$me['id'].'/picture',
+        ]
+        return $userProfile;
+    }
+
+    public function getAlbums()
+    {
+        $fbAlbums = $this->facebook->api('/me/albums', 'GET')['data'];
+        $albums = [];
+        foreach($fbAlbums as $fbAlbum) { 
+            $r = $this->facebook->api('/'.$fbAlbum['id'].'/picture?redirect=false', 'GET');
+            $thumbnailPath = $r['data']['url'];
+
+            $album = [
+                'id'            => $fbAlbum['id']
+                'name'          => $fbAlbum['name'],
+                'thumbnailPath' => $thumbnailPath,
+            ];
+
+            array_push($albums, $album);
+        }
+        return $albums;
+    }
+
+    public function getImagesInAlbum($albumId)
+    {
+        $fbImages = $this->facebook->api('/'.$albumId.'/photos', 'GET')['data'];
+        $images = [];
+        foreach($fbImages as $fbImage) {
+            $image = [
+                'id'        => $fbImage['id'],
+                'imagePath' => $fbImage['picture'],
+            ];
+            array_push($images, $image);
+        }
+        return $images;
+    }
+
+    public function getFriends()
+    {
+        $fbFriends = $this->facebook->api('/me/friends');
+        $friends = [];
+        foreach($fbFriends as $fbFriend) {
+            $friend = [
+                'id'            => $fbFriend['id'],
+                'name'          => $fbFriend['name'],
+                'iconImagePath' => 'https://graph.facebook.com/'.$me['id'].'/picture',
+            ];
+            array_push($friends, $friend);
+        }
+        return $friends;
     }
 }
