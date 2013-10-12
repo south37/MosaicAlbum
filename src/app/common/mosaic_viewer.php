86a67d176ba3cfc4ae0a9de1faecf4c4adbbbebb
masa <masa@cyber.t.u-tokyo.ac.jp>
2013-10-13 01:34:11 +0900

    mosaic viewer 処理の流れをメモしたよ

--- src/app/common/mosaic_viewer.php
+++ src/app/common/mosaic_viewer.php
@@ -9,6 +9,23 @@
 
 ゴールIDをもとにアクセスされたときの処理:{
   $app->get('/common/mosaic_viewer/:goalID', function($goalID) use ($app){
+
+    #セキュリティ処理
+
+
+    #参加ユーザリスト作成
+
+
+    #参加ユーザ：FBアイコンパス取得
+
+
+    #モザイク画像取得
+
+
+    #モザイクピースのリストを取得
+
+
+    #画面レンダリング
     $app->render('common/mosaic_viewer.html.twig',['goalId' => $goalID]);
   })
     ->name('mosaic_viewer')
