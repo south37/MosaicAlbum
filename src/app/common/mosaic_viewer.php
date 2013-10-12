38dc73db60c644b89dc7172c9c470878f1d254c1
masa <masa@cyber.t.u-tokyo.ac.jp>
2013-10-12 20:59:53 +0900

    make mosaic viewer base

--- src/app/common/mosaic_viewer.php
+++ src/app/common/mosaic_viewer.php
@@ -1,8 +1,16 @@
 <?php
-//parameter無しのget
-$app->get('/common/mosaic_viewer', function() use ($app) {
-  $app->render('common/mosaic_viewer.html.twig');
-})
-  ->name('mosaic_viewer')
-  ;
+ページ読み込みテスト:{
+  $app->get('/common/mosaic_viewer/test', function() use ($app) {
+    $app->render('common/mosaic_viewer.html.twig');
+  })
+    ->name('mosaic_viewer_test')
+    ;
+}
 
+ゴールIDをもとにアクセスされたときの処理:{
+  $app->get('/common/mosaic_viewer/:goalID', function($goalID) use ($app){
+    $app->render('common/mosaic_viewer.html.twig',['goalId' => $goalID]);
+  })
+    ->name('mosaic_viewer')
+    ;
+}
