b87886e41a57b2eb72d8c8d8f58c50adae251524
masa <masa@cyber.t.u-tokyo.ac.jp>
2013-10-13 02:31:21 +0900

    album_select_guest:create_mosaicボタンの追加．ルーティングだけ確認．中身 無し

--- src/app/guest/album_select_guest.php
+++ src/app/guest/album_select_guest.php
@@ -1,8 +1,22 @@
 <?php
 //parameter無しのget
 $app->get('/guest/album_select_guest', function() use ($app) {
+
+
   $app->render('guest/album_select_guest.html.twig');
 })
   ->name('album_select_guest')
   ;
 
+$app->get('/guest/album_select_guest/create', function() use ($app){
+  print "create!:";
+
+  # 1.repository
+  #
+
+
+  $app->render('guest/album_select_guest.html.twig');
+
+})
+  ->name('create_mosaic')
+  ;
