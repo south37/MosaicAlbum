85c5ea5b571ed807f773bbc763f7f4eb5ffe6439
nantyoku <syukatsusouth@gmail.com>
2013-10-14 12:23:19 +0900

    FBHelperのバグフィクス & start_masterまわり

--- src/app/master/start_master.php
+++ src/app/master/start_master.php
@@ -1,6 +1,8 @@
 <?php
 //parameter無しのget
-$app->get('/master/start_master', function() use ($app) {
+$app->get('/master/start_master', function() use ($app, $container) {
+      $userId = $container['FBHelper']->getUserId();
+      echo $userId;
   $app->render('master/start_master.html.twig');
 })
   ->name('start_master')
