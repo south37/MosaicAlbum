4d61f42bdac09630c25d9449d694ad28f75ccc12
moguranosenshi <moguranosenshi@gmail.com>
2013-10-18 02:00:01 +0900

    アルバム選択とアルバム閲覧を分けた

--- src/app/common/album_viewer.php
+++ src/app/common/album_viewer.php
@@ -17,35 +17,7 @@ $app->get('/common/album_viewer/:goalImageId', function($goalImageId) use ($app,
 			array_push($imagePathList, $image['imagePath']);
 		}
 	}
-	# アルバムリスト（アルバム選択画面で表示する）
-	$albumList = [];
-	# 自分のFacebookアルバムのリストを取得
-	$fbAlbums = $container['FBHelper']->getAlbums();
-	foreach ($fbAlbums as $fbAlbum) {
-		# アルバムの写真一覧を取得（fbImageId, imagePath）
-		$images = $container['FBHelper']->getImagesInAlbum($fbAlbum['id']);
-		# アルバムリストにアルバムの写真一覧を保存
-		$fbAlbum['images'] = $images;
-		array_push($albumList, $fbAlbum);
-	}
-	$app->render('common/album_viewer.html.twig',
-  		["imagePathList"=>$imagePathList, "albumList"=>$albumList]);
+	$app->render('common/album_viewer.html.twig', ["imagePathList"=>$imagePathList]);
 })
 	->name('album_viewer')
-	;
-
-// アルバムを追加
-$app->post('/common/album_viewer', function() use ($app,$container) {
-	# 追加するアルバム
-    $fbAlbum = $app->request()->post();
-	# DBに登録
-	$userId = $container['session']->get('userId');
-	$data = array('id' => 0, 'user_id' => $userId, 'goal_image_id' => $goalImageId, 'fb_album_id' => $fbAlbum['id']);
-	$album = new \Vg\Model\Album();
-	$album->setProperties($data);
-	$albumId = $container['repository.album']->insert($album);
-	# アルバムビューアへ
-	$app->redirect($app->urlFor('album_viewer', ['goalImageId'=>$container['session']->get('goalImageId')]));
-})
-	->name('add_album')
 	;
--- src/app/guest/album_select_guest.php
+++ src/app/guest/album_select_guest.php
@@ -6,13 +6,37 @@ include('../src/mosaic/Image.php');
 include('../src/mosaic/gd_bmp_util.php');
 
 
-//parameter無しのget
-$app->get('/guest/album_select_guest', function() use ($app) {
-
-
-  $app->render('guest/album_select_guest.html.twig');
+// アルバムセレクト＿ゲスト：使うアルバムを選択
+$app->get('/guest/album_select_guest', function() use ($app, $container) {
+  $albumList = [];
+  # 自分のFacebookアルバムのリストを取得
+  $fbAlbums = $container['FBHelper']->getAlbums();
+  foreach ($fbAlbums as $fbAlbum) {
+    # アルバムの写真一覧を取得（fbImageId, imagePath）
+    $images = $container['FBHelper']->getImagesInAlbum($fbAlbum['id']);
+    # アルバムリストにアルバムの写真一覧を保存
+    $fbAlbum['images'] = $images;
+    array_push($albumList, $fbAlbum);
+  }
+  $app->render('guest/album_select_guest.html.twig', ["albumList"=>$albumList]);
 })
   ->name('album_select_guest')
+  ;
+
+// アルバムを追加
+$app->post('/guest/album_select_guest', function() use ($app, $container) {
+  # 追加するアルバム
+    $fbAlbum = $app->request()->post();
+  # DBに登録
+  $userId = $container['session']->get('userId');
+  $data = array('id' => 0, 'user_id' => $userId, 'goal_image_id' => $goalImageId, 'fb_album_id' => $fbAlbum['id']);
+  $album = new \Vg\Model\Album();
+  $album->setProperties($data);
+  $albumId = $container['repository.album']->insert($album);
+  # アルバムビューアへ
+  $app->redirect($app->urlFor('album_viewer', ['goalImageId'=>$container['session']->get('goalImageId')]));
+})
+  ->name('add_album')
   ;
 
 
