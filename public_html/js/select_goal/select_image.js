c462069ae6f72d743b83cc99d3c22e852efb8714
nantyoku <syukatsusouth@gmail.com>
2013-10-20 04:17:55 +0900

    選択したゴール写真をsubmit可能に

--- public_html/js/select_goal/select_image.js
+++ public_html/js/select_goal/select_image.js
@@ -3,7 +3,12 @@
 })();
 
 function selectImage() {
-  var opener = window.dialogArguments;
+  var select  = document.getElementsByTagName('select')[0];
+  var options = select.options;
+  var goalImageId = options.item(select.selectedIndex).value;
+
+  var input   = window.opener.document.getElementById('goalImageId');
+  input.value = goalImageId;
 
   window.close();
 }
