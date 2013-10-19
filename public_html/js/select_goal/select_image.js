fb8754f1c4b9156363abc833b46e9a5566f25e30
nantyoku <syukatsusouth@gmail.com>
2013-10-20 03:36:34 +0900

    ボタンを押すとダイアログが閉じる

---
+++ public_html/js/select_goal/select_image.js
@@ -0,0 +1,9 @@
+(function () {
+  $(".images").imagepicker({hide_select: false, show_label: false});
+})();
+
+function selectImage() {
+  var opener = window.dialogArguments;
+
+  window.close();
+}
