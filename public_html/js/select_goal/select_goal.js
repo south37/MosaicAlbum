cd7c9397cbc0a2c8610b6016d4ff5b4f3547ac7d
nantyoku <syukatsusouth@gmail.com>
2013-10-20 02:11:51 +0900

    albumsページにアルバムのサムネイル写真を表示

--- public_html/js/select_goal/select_goal.js
+++ public_html/js/select_goal/select_goal.js
@@ -1,7 +1,10 @@
-function selectGoalImage() {
+function showAlbums() {
     window.showModalDialog(
         '/master/albums',
         this,  //ダイアログに渡すパラメータ（この例では、自分自身のwindowオブジェクト）
         "dialogWidth=800px; dialogHeight=480px;"
     );
+}
+
+function showImages() {
 }
--- src/app/master/select_goal.php
+++ src/app/master/select_goal.php
@@ -13,6 +13,10 @@ $app->get('/master/albums', function () use ($app, $container) {
         "id"            => "400193440110501",
         "name"          => "2013年秋日本物理学会",
         "thumbnailPath" => "https://fbcdn-photos-g-a.akamaihd.net/hphotos-ak-prn2/1378561_400194690110376_1611527405_s.jpg"
+    ], [
+        "id"            => "367905490005963",
+        "name"          => "Profile Pictures",
+        "thumbnailPath" => "https://fbcdn-photos-e-a.akamaihd.net/hphotos-ak-ash3/533896_367905493339296_1533302163_s.jpg"    
     ]];
     $app->render('master/albums.html.twig', ['fbAlbums' => $fbAlbums]);
 })
