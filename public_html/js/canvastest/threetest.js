2ef4fe9eab55d145dc3b6dd7d3f9123db27f4f09
masa <masa@cyber.t.u-tokyo.ac.jp>
2013-10-19 22:54:34 +0900

    mosaic_viewer:画像いっぱい表示1

--- public_html/js/canvastest/threetest.js
+++ public_html/js/canvastest/threetest.js
@@ -2,7 +2,7 @@
 $(function() {
   console.log("load threetest.coffee");
   return window.addEventListener("DOMContentLoaded", function() {
-    var anim, aspect, camera, cubeMesh, directioalLight, far, fov, geometry, height, i, material, near, planeMesh, render, scene, tex, theta, tmesh, width, _i;
+    var anim, aspect, camera, controls, cubeMesh, directioalLight, far, fov, geometry, height, i, isEnableMove, j, material, materials, miku_tex, near, path, pathList, pclientX, pclientY, piece, planeMesh, render, scene, tex, texlist, theta, tmesh, width, _i, _j, _k;
     console.log("load window");
     width = window.innerWidth;
     height = window.innerHeight;
@@ -14,19 +14,43 @@ $(function() {
     fov = 80;
     aspect = width / height;
     near = 1;
-    far = 1000;
+    far = 10000;
     camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 500;
     camera.position.x = 100;
     scene.add(camera);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
+    controls = new THREE.TrackballControls(camera, render.domElement);
+    controls.rotateSpeed = 0.5;
+    controls.addEventListener('change', render);
     directioalLight = new THREE.DirectionalLight(0xffffff, 3);
     directioalLight.position.z = 300;
     scene.add(directioalLight);
-    tex = new THREE.ImageUtils.loadTexture('/img/miku.jpg');
+    miku_tex = new THREE.ImageUtils.loadTexture('/img/miku.jpg');
+    pathList = ["resize_0.png", "resize_1.png", "resize_2.png", "resize_3.png", "resize_4.png", "resize_5.png", "resize_6.png", "resize_7.png", "resize_8.png", "resize_9.jpg"];
+    texlist = (function() {
+      var _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = pathList.length; _i < _len; _i++) {
+        path = pathList[_i];
+        _results.push(new THREE.ImageUtils.loadTexture('/img/resize_img/' + path));
+      }
+      return _results;
+    })();
+    materials = (function() {
+      var _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = texlist.length; _i < _len; _i++) {
+        tex = texlist[_i];
+        _results.push(new THREE.MeshBasicMaterial({
+          map: tex
+        }));
+      }
+      return _results;
+    })();
     geometry = new THREE.CubeGeometry(20, 20, 20);
     material = new THREE.MeshLambertMaterial({
-      map: tex
+      map: miku_tex
     });
     cubeMesh = new THREE.Mesh(geometry, material);
     scene.add(cubeMesh);
@@ -38,20 +62,54 @@ $(function() {
     }
     geometry = new THREE.PlaneGeometry(500, 500, 1, 1);
     material = new THREE.MeshBasicMaterial({
-      map: tex
+      map: miku_tex
     });
     planeMesh = new THREE.Mesh(geometry, material);
     scene.add(planeMesh);
-    render.render(scene, camera);
+    geometry = new THREE.PlaneGeometry(100, 100, 1, 1);
+    for (i = _j = 0; _j <= 10; i = ++_j) {
+      for (j = _k = 0; _k <= 10; j = ++_k) {
+        console.log(i, ":", j);
+        piece = new THREE.Mesh(geometry, materials[(i + j) % 10]);
+        piece.position.set(100 * i - 500, 100 * j - 500, -10 * (i + j));
+        scene.add(piece);
+      }
+    }
+    isEnableMove = false;
+    pclientX = 0;
+    pclientY = 0;
+    $('canvas').mousedown(function() {
+      console.log("mousedown");
+      return isEnableMove = true;
+    });
+    $('canvas').mouseup(function() {
+      console.log("mouseup");
+      return isEnableMove = false;
+    });
+    $('canvas').mousemove(function(e) {
+      var diff;
+      console.log("mousemove");
+      if (isEnableMove) {
+        console.log("enable");
+        diff = new THREE.Vector3(-e.clientX + pclientX, -e.clientY + pclientY, 0);
+        camera.position.add(diff);
+      }
+      pclientX = e.clientX;
+      return pclientY = e.clientY;
+    });
     theta = 0;
     anim = function() {
       var rad;
+      requestAnimationFrame(anim);
       rad = theta * Math.PI / 180.0;
       cubeMesh.rotation.set(rad, rad, rad);
       theta++;
       render.render(scene, camera);
-      return requestAnimationFrame(anim);
+      controls.update();
+      return true;
     };
-    return anim();
+    render.render(scene, camera);
+    anim();
+    return true;
   });
 });
