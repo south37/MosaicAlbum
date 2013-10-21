5140f84ecf093fba541446b165c44116904e7447
nantyoku <syukatsusouth@gmail.com>
2013-10-21 23:27:58 +0900

    imgの保存先を修正

--- src/Vg/Repository/FBHelperRepository.php
+++ src/Vg/Repository/FBHelperRepository.php
@@ -197,7 +197,7 @@ class FBHelperRepository
     public function downloadImage($imagePath)
     {
         $image = file_get_contents($imagePath);
-        $savePath = '/img/resource_img/'.basename($imagePath);
+        $savePath = __DIR__.'/../../public_html/img/resource_img/'.basename($imagePath);
 
         file_put_contents($savePath, $image);
 
