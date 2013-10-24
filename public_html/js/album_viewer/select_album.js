fd8bcea0527bf07732b87ba7dbdf3d0130eed61c
nantyoku <syukatsusouth@gmail.com>
2013-10-25 02:53:49 +0900

    album_viewerにおけるmodalをバツボタンで消した際にもpostされるバグを修正

--- public_html/js/album_viewer/select_album.js
+++ public_html/js/album_viewer/select_album.js
@@ -1,8 +1,11 @@
 function showAlbums() {
-    window.showModalDialog(
-        '/album_viewer/modal',
-        this,  //ダイアログに渡すパラメータ（この例では、自分自身のwindowオブジェクト）
-        "dialogWidth=800px; dialogHeight=480px;"
-    );
+  window.showModalDialog(
+    '/album_viewer/modal',
+    this,  //ダイアログに渡すパラメータ（この例では、自分自身のwindowオブジェクト）
+    "dialogWidth=800px; dialogHeight=480px;"
+  );
+  var input = document.getElementById('albumId');
+  if (input.value !== '') {
     document.getElementById('album-frm').submit();
+  }
 }
