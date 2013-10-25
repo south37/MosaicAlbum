ccf229d2d9accdb209b5416fa9ef996e9a299167
NewWife <wega315@gmail.com>
2013-10-25 22:55:53 +0900

    DataBaseを初期化するshファイルを追加.

---
+++ resetDB.sh
@@ -0,0 +1,8 @@
+# Reset DataBase
+cp .dbup/properties.ini .dbup/properties.ini.tmp
+rm .dbup/applied/V*
+php dbup.phar init
+rm .dbup/properties.ini
+cp .dbup/properties.ini.tmp .dbup/properties.ini
+rm .dbup/properties.ini.tmp
+php dbup.phar up
